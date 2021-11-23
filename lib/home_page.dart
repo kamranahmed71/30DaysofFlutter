5cd9785251d283e84202a63cb97b0fc799d61360
Kamran Ahmed <kamranferoze71@gmail.com>
2021-11-23 10:48:56 +0500

    Day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final int days = 10;
+    final String name = "Codepur";
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of flutter by $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
