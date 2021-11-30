4d9e95304e63601d0a502ca8c2e4968ff71f3dd5
Kamran Ahmed <kamranferoze71@gmail.com>
2021-11-30 11:40:58 +0500

    Day 5 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/utils/routes.dart';
 
 class LoginPage extends StatelessWidget {
   LoginPage({Key? key}) : super(key: key);
@@ -7,64 +8,67 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset(
-            "assets/images/login_image.png",
-            fit: BoxFit.fill,
-            width: 200,
-            height: 200,
-          ),
-
-          SizedBox(
-            height: 15.0,
-          ),
-
-          Text(
-            "Welcome",
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_image.png",
+              fit: BoxFit.fill,
+              width: 200,
+              height: 200,
             ),
-          ),
 
-          SizedBox(
-            height: 15.0,
-          ),
-
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
-//            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                      hintText: "Enter username", labelText: "Username"),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      hintText: "Enter password", labelText: "Password"),
-                ),
-              ],
+            SizedBox(
+              height: 15.0,
             ),
-          ),
 
-          SizedBox(
-            height: 20.0,
-          ),
+            Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
 
-          ElevatedButton(
-            onPressed: () {
-              print("Hi Codepur");
-            },
-            child: Text("Login"),
-          ),
+            SizedBox(
+              height: 15.0,
+            ),
 
-          //    Image(
-          //      image: AssetImage("/assets/images/login_image.png"),), // Image
-        ], // Children
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 18.0, horizontal: 18.0),
+              //            padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Enter username", labelText: "Username"),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: "Enter password", labelText: "Password"),
+                  ),
+                ],
+              ),
+            ),
+
+            SizedBox(
+              height: 40.0,
+            ),
+
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, MyRoutes.homeRoute);
+              },
+              child: Text("Login"),
+              style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+            ),
+
+            //    Image(
+            //      image: AssetImage("/assets/images/login_image.png"),), // Image
+          ], // Children
+        ),
       ),
     );
   }
